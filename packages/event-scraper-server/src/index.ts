b45198dd36d9d6a2d645eddd56f801705feb61e4
Dominic Kolbe <kolbedominic@gmail.com>
2020-09-10 00:32:30 +0100

    Add simple server setup

--- packages/event-scraper-server/src/index.ts
+++ packages/event-scraper-server/src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello server");
+require("dotenv-safe").config();
+
+import Koa from "koa";
+import Router from "koa-router";
+
+const PORT = process.env.PORT;
+
+const server = async () => {
+  const app = new Koa();
+  const router = new Router();
+
+  router.get("/api", async (ctx) => {
+    ctx.body = "Hello World.";
+  });
+
+  app.use(router.routes());
+
+  app.listen(PORT, () => {
+    console.log(`[Info]: server is running on port ${PORT}`);
+  });
+};
+
+server();
